e3e753fd0a1b8fa65895af536b5c8b268ef03b8a
SeunghoHa <sh.ha1018@gmail.com>
2020-04-04 23:48:05 +0900

    added ffmpeg-lib

--- main.cpp
+++ main.cpp
@@ -2,9 +2,17 @@
 
 #include <QApplication>
 
+extern "C" {
+#include <libavcodec/avcodec.h>
+
+}
+
+#include <qdebug.h>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    qDebug() << avcodec_configuration();
     MainWindow w;
     w.show();
     return a.exec();
